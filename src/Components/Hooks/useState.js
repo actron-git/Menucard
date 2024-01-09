9897c38232c1d72a5801c86c44caa4c575cef0a8
Ajit Gavade <ajitgavade73@gmail.com>
2024-01-09 16:57:42 +0530

    code pushed

---
+++ src/Components/Hooks/useState.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style.css'
+
+const useState = () => {
+  return (
+    <>
+        <div>
+            
+            <div class='button2'>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Increment
+            </div>
+        </div>
+        <div>
+            
+            <div class='button2'>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Decrement
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default useState
